8d1467b5bd6b193e262eca683f51e337cc8802fa
Martin Müllenhaupt <mm+git@netlair.de>
2017-05-08 09:50:34 +0200

    beta client: make message about losing stats more clear

--- src/client/__init__.py
+++ src/client/__init__.py
@@ -21,7 +21,7 @@ if not 'CI' in os.environ:
     from PyQt4.QtGui import QMessageBox
     QMessageBox.information(None, "BETA client information",
                             "<p>This client always connects to the <b>test server</b>.</p>"
-                            "<p>You can't play or chat with non-beta players and all stats will be lost.</p>")
+                            "<p>You can't play or chat with non-beta players and no game statistics are kept.</p>")
 
 class ClientState(IntEnum):
     '''
